b37c19146f42e902b26b3887f32e50141e98a924
Maryam Wardak <mmwk183@gmail.com>
2024-06-28 16:12:28 +0100

    Starting form

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,13 @@
+function Form() {
+    return (
+        <form>
+            <label>Total bill</label>
+            <label>Number of people</label>
+            <label>Tip percentage</label>
+            <input> </input>
+        </form>
+
+    )
+} 
+
+export default Form
